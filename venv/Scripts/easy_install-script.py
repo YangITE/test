b5e909b456cd8b550fbb4538b76ed8d689f5b4f3
test7@CYNS.LOCAL <test7@CYNS.LOCAL>
2019-05-14 14:45:21 +0800

    Initial commit

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!"F:\ITE\ITE Higher Nitec in Cyber & Network Security\Year 2\5194\Applied Scripting Using Python\Labs for Submission\Lab 6\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+    )
---
+++ venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"F:\ITE\ITE Higher Nitec in Cyber & Network Security\Year 2\5194\Applied Scripting Using Python\Labs for Submission\Lab 6\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+    )
